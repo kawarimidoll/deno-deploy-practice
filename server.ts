80c46788376837f4985c3f66dfaf43c15516b8b8
kawarimidoll <kawarimidoll+git@gmail.com>
2021-07-03 17:39:34 +0900

    add parameter handling

--- server.ts
+++ server.ts
@@ -1,7 +1,22 @@
 /// <reference path="./deploy.d.ts" />
 
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello Deno Deploy!", {
+  const { host, pathname, searchParams } = new URL(event.request.url);
+  const params = Object.fromEntries([...searchParams.entries()]);
+
+  console.log({ host, pathname, params });
+
+  const message = "Hello Deno Deploy!";
+
+  if (pathname.endsWith(".json")) {
+    const response = new Response(JSON.stringify({ message, params }), {
+      headers: { "content-type": "application/json" },
+    });
+    event.respondWith(response);
+    return;
+  }
+
+  const response = new Response(message + " from " + pathname, {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
