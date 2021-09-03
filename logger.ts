192ede2b829e0db541a40a862ffe4725e65c75a9
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-03 20:52:45 +0900

    update logger.ts

--- logger.ts
+++ logger.ts
@@ -1,38 +1,50 @@
-import * as log from "https://deno.land/std@0.99.0/log/mod.ts";
-import { LogRecord } from "https://deno.land/std@0.99.0/log/logger.ts";
+import {
+  blue,
+  bold,
+  red,
+  yellow,
+} from "https://deno.land/std@0.106.0/fmt/colors.ts";
 
-const filename = "./app.log";
-const formatter = (logRecord: LogRecord) => {
-  const { datetime, levelName, msg } = logRecord;
-
+function toISO8601String(datetime: Date): string {
   const d = new Date(datetime.getTime() - datetime.getTimezoneOffset() * 6e4);
-  const logTime = d.toISOString().slice(0, -5) +
+  return d.toISOString().slice(0, -5) +
     d.toString().replace(/^.*GMT([-+]\d{2})(\d{2}).*$/, "$1:$2");
+}
 
-  return `${logTime} ${levelName.padEnd(7)} ${msg}`;
+const logStyles = {
+  debug: (msg: string) => msg,
+  info: blue,
+  warn: yellow,
+  error: red,
+  critical: (msg: string) => bold(red(msg)),
+};
+type LevelName = keyof typeof logStyles;
+
+function output(date: Date, logLevel: LevelName, msg: unknown[]) {
+  const logger = logLevel === "critical" ? console.error : console[logLevel];
+  logger(
+    logStyles[logLevel](`${toISO8601String(date)} ${logLevel.toUpperCase()}`),
+  );
+  logger(...msg);
+  logger();
+}
+
+const log = {
+  debug: (...msg: unknown[]) => {
+    output(new Date(), "debug", msg);
+  },
+  info: (...msg: unknown[]) => {
+    output(new Date(), "info", msg);
+  },
+  warn: (...msg: unknown[]) => {
+    output(new Date(), "warn", msg);
+  },
+  error: (...msg: unknown[]) => {
+    output(new Date(), "error", msg);
+  },
+  critical: (...msg: unknown[]) => {
+    output(new Date(), "critical", msg);
+  },
 };
 
-await log.setup({
-  handlers: {
-    console: new log.handlers.ConsoleHandler("DEBUG", {
-      formatter,
-    }),
-
-    file: new log.handlers.FileHandler("DEBUG", {
-      filename,
-      formatter,
-    }),
-  },
-
-  loggers: {
-    default: {
-      level: "DEBUG",
-      handlers: ["console", "file"],
-    },
-  },
-});
-
-const Logger = log.getLogger();
-console.log(`logfile: ${filename}`);
-
-export { Logger };
+export { log };
