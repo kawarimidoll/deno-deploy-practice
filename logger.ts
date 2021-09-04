3e1622465cca3d9e6b0ba095487c0c6c721c857b
kawarimidoll <kawarimidoll+git@gmail.com>
2021-09-04 12:01:54 +0900

    update logger.ts

--- logger.ts
+++ logger.ts
@@ -4,72 +4,68 @@ import {
   reset,
   yellow,
 } from "https://deno.land/std@0.106.0/fmt/colors.ts";
+import { datetime } from "https://deno.land/x/ptera@v1.0.0-beta/mod.ts";
 
-export function toISO8601String(datetime: Date): string {
-  const d = new Date(datetime.getTime() - datetime.getTimezoneOffset() * 6e4);
-  return d.toISOString().slice(0, -5) +
-    d.toString().replace(/^.*GMT([-+]\d{2})(\d{2}).*$/, "$1:$2");
-}
-
-export const LOG_LEVELS = {
-  debug: {
-    color: reset,
-    symbol: "✔",
-  },
-  info: {
-    color: blue,
-    symbol: "ℹ",
-  },
-  warn: {
-    color: yellow,
-    symbol: "⚠",
-  },
-  error: {
-    color: red,
-    symbol: "✖",
-  },
-};
-
-export type LevelName = keyof typeof LOG_LEVELS;
+export type LevelName = keyof typeof Log.LEVELS;
 
 export class Log {
-  private timeFmt = toISO8601String;
+  static readonly LEVELS = {
+    debug: {
+      color: reset,
+      symbol: "✔",
+    },
+    info: {
+      color: blue,
+      symbol: "ℹ",
+    },
+    warn: {
+      color: yellow,
+      symbol: "⚠",
+    },
+    error: {
+      color: red,
+      symbol: "✖",
+    },
+  };
+
+  private fmt: string;
   private levelSign: (logLevel: LevelName) => string;
-  private suffix = "";
+  private suffix: string;
 
   constructor({
     minLogLevel = "debug",
-    signType = "symbol",
-    showTimestamp = true,
+    levelIndicator = "symbol",
+    datetimeFormat = "YYYY-MM-ddTHH:mm:ssZ",
     addNewLine = false,
   }: {
     minLogLevel?: LevelName;
-    signType?: "none" | "full" | "initial" | "symbol";
-    showTimestamp?: boolean;
+    levelIndicator?: "none" | "full" | "initial" | "symbol";
+    datetimeFormat?: string;
     addNewLine?: boolean;
   } = {}) {
-    const levels: LevelName[] = ["debug", "info", "warn", "error"];
-    for (const level of levels) {
+    for (const level of Object.keys(Log.LEVELS) as LevelName[]) {
       if (minLogLevel === level) break;
       this[level] = () => ({});
     }
 
-    if (!showTimestamp) {
-      this.timeFmt = () => "";
-    }
+    this.fmt = datetimeFormat;
     this.suffix = addNewLine ? "\n" : "";
 
     this.levelSign = {
       none: () => "",
       full: (logLevel: LevelName) => " " + logLevel.toUpperCase().padEnd(5),
       initial: (logLevel: LevelName) => " " + logLevel[0].toUpperCase(),
-      symbol: (logLevel: LevelName) => " " + LOG_LEVELS[logLevel].symbol,
-    }[signType];
+      symbol: (logLevel: LevelName) => " " + Log.LEVELS[logLevel].symbol,
+    }[levelIndicator];
+  }
+
+  timestamp(date: Date) {
+    return datetime(date).format(this.fmt);
   }
 
   output(date: Date, logLevel: LevelName, msg: unknown[]) {
-    const prefix = LOG_LEVELS[logLevel].color(
-      `${this.timeFmt(date)}${this.levelSign(logLevel)}`.trimStart(),
+    const prefix = Log.LEVELS[logLevel].color(
+      `${this.timestamp(date)}${this.levelSign(logLevel)}`.trimStart(),
     );
 
     console[logLevel](prefix, ...msg, this.suffix);
